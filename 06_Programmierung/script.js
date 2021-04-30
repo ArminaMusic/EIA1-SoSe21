220ad398756f2c0ea54a74942977eb4e53e915d1
Armina Music <armina.music@hs-furtwangen.de>
2021-04-30 14:54:02 +0200

    initat commit

--- 06_Programmierung/script.js
+++ 06_Programmierung/script.js
@@ -1,59 +1,45 @@
-var countryAfrica = "Afrika";
-var countrySouthamerica = "Südamerika";
-var countryEurope = "Europa";
-var countryNorthamerica = "Nordamerika";
-var countryAsia = "Asien";
-var countryAustralia = "Australien";
+//Africa
+var countryAfrica = "Africa";
 var africa2008 = 1028;
 var africa2018 = 1235.5;
-var southamerica2008 = 1132.6;
-var southamerica2018 = 1261.5;
+//Southamerica
+var countrySouthamerica = "Southamerica";
+var southAmerica2008 = 1132.6;
+var southAmerica2018 = 1261.5;
+//Europa
+var countryEurope = "Europe";
 var europe2008 = 4965.7;
 var europe2018 = 4209.3;
-var northamerica2008 = 6600.4;
-var northamerica2018 = 6035.6;
+//Northamerica
+var countryNorthamerica = "Northamerica";
+var northAmerica2008 = 6600.4;
+var northAmerica2018 = 6035.6;
+//Asia
+var countryAsia = "Asia";
 var asia2008 = 12954.7;
 var asia2018 = 16274.2;
+//Australia
+var countryAustralia = "Australia";
 var australia2008 = 1993;
 var australia2018 = 2100.5;
-var all = africa2018 + southamerica2018 + europe2018 + northamerica2018 + asia2018 + australia2018;
-//Berechnet die Relativen Gesamtemissionen der Welt
-function calcGesamtemission1(zahl1, zahl2) {
-    var ergebniss = zahl1 / zahl2 * 100;
-    return ergebniss.toFixed(2);
+//World
+var all = africa2018 + southAmerica2018 + europe2018 + northAmerica2018 + asia2018 + australia2018;
+//Emission
+function emission(countryName, emissionen2018, emission2008) {
+    document.querySelector(".country").innerHTML = countryName;
+    document.querySelector(".emissionen2018").innerHTML = emissionen2018.toString();
+    document.querySelector(".relativeToWorld").innerHTML = Math.round(emissionen2018 / all * 100 * 100) / 100 + "%";
+    document.querySelector(".growthRate").innerHTML = Math.round((emissionen2018 - emission2008) / emission2008 * 100 * 100 / 100) + "%";
+    document.querySelector(".growthRateAbsolute").innerHTML = "" + Math.round((emissionen2018 - emission2008) * 100) / 100;
+    document.querySelector(".chart").style.height = Math.round((emissionen2018 / all * 100 * 100) / 100) + "%";
 }
-//Berechnet die Veränderung 
-function calcVerändert1(zahl1, zahl2) {
-    var ergebniss = (zahl1 - zahl2) / zahl2 * 100;
-    return ergebniss.toFixed(2);
-}
-//Berechnet die Differenz 
-function calcVergleich1(zahl1, zahl2) {
-    var ergebniss = zahl1 - zahl2;
-    return ergebniss.toFixed(2);
-}
-console.log("Die Emission von " + countryAfrica + " ist: " + africa2018 + " kg CO2");
-console.log("Die Emission von " + countrySouthamerica + " ist: " + southamerica2018 + " kg CO2");
-console.log("Die Emission von " + countryEurope + " ist: " + europe2018 + " kg CO2");
-console.log("Die Emission von " + countryNorthamerica + " ist: " + northamerica2018 + " kg CO2");
-console.log("Die Emission von " + countryAsia + " ist: " + asia2018 + " kg CO2");
-console.log("Die Emission von " + countryAustralia + " ist: " + australia2018 + " kg CO2");
-console.log("Relativ zu Gesamtemission der Welt verursacht " + countryAfrica + " damit " + calcGesamtemission1(africa2018, all) + " %");
-console.log("Relativ zu Gesamtemission der Welt verursacht " + countrySouthamerica + " damit " + calcGesamtemission1(southamerica2018, all) + " %");
-console.log("Relativ zu Gesamtemission der Welt verursacht " + countryEurope + " damit " + calcGesamtemission1(europe2018, all) + " %");
-console.log("Relativ zu Gesamtemission der Welt verursacht " + countryNorthamerica + " damit " + calcGesamtemission1(northamerica2018, all) + " %");
-console.log("Relativ zu Gesamtemission der Welt verursacht " + countryAsia + " damit " + calcGesamtemission1(asia2018, all) + " %");
-console.log("Relativ zu Gesamtemission der Welt verursacht " + australia2018 + " damit " + calcGesamtemission1(australia2018, all) + " %");
-console.log("F\u00FCr " + countryAfrica + " hat sich 2018 im Vergleich zu 2008 die Emission um " + calcVerändert1(africa2018, africa2008) + " % ver\u00E4ndert");
-console.log("F\u00FCr " + countrySouthamerica + " hat sich 2018 im Vergleich zu 2008 die Emission " + calcVerändert1(southamerica2018, southamerica2008) + " % ver\u00E4ndert");
-console.log("F\u00FCr " + countryEurope + " hat sich 2018 im Vergleich zu 2008 die Emission um " + calcVerändert1(europe2018, europe2008) + " % ver\u00E4ndert");
-console.log("F\u00FCr " + countryNorthamerica + " hat sich 2018 im Vergleich zu 2008 die Emission um " + calcVerändert1(northamerica2018, northamerica2008) + " % ver\u00E4ndert");
-console.log("F\u00FCr " + countryAsia + " hat sich 2018 im Vergleich zu 2008 die Emission um " + calcVerändert1(asia2018, asia2008) + " % ver\u00E4ndert");
-console.log("F\u00FCr " + countryAustralia + " hat sich 2018 im Vergleich zu 2008 die Emission um " + calcVerändert1(australia2018, australia2008) + " % ver\u00E4ndert");
-console.log("2018 im Vergleich zu 2008 sind das " + calcVergleich1(africa2018, africa2008) + " kg CO2");
-console.log("2018 im Vergleich zu 2008 sind das " + calcVergleich1(southamerica2018, southamerica2008) + " kg CO2");
-console.log("2018 im Vergleich zu 2008 sind das " + calcVergleich1(europe2018, europe2008) + " kg CO2");
-console.log("2018 im Vergleich zu 2008 sind das " + calcVergleich1(northamerica2018, northamerica2008) + " kg CO2");
-console.log("2018 im Vergleich zu 2008 sind das " + calcVergleich1(asia2018, asia2008) + " kg CO2");
-console.log("2018 im Vergleich zu 2008 sind das " + calcVergleich1(australia2018, australia2008) + " kg CO2");
+;
+window.addEventListener("load", function () {
+    document.querySelector(".europe").addEventListener("click", function () { emission(countryEurope, europe2018, europe2008); });
+    document.querySelector(".northamerica").addEventListener("click", function () { emission(countryNorthamerica, northAmerica2018, northAmerica2008); });
+    document.querySelector(".southamerica").addEventListener("click", function () { emission(countrySouthamerica, southAmerica2018, southAmerica2008); });
+    document.querySelector(".africa").addEventListener("click", function () { emission(countryAfrica, africa2018, africa2008); });
+    document.querySelector(".asia").addEventListener("click", function () { emission(countryAsia, asia2018, asia2008); });
+    document.querySelector(".australia").addEventListener("click", function () { emission(countryAustralia, africa2018, africa2008); });
+});
 //# sourceMappingURL=script.js.map
--- 06_Programmierung/script.ts
+++ 06_Programmierung/script.ts
@@ -1,65 +1,45 @@
-var countryAfrica: string = "Afrika";
-var countrySouthamerica: string ="Südamerika";
-var countryEurope: string = "Europa";
-var countryNorthamerica: string = "Nordamerika";
-var countryAsia: string = "Asien";
-var countryAustralia: string = "Australien";
+//Africa
+var countryAfrica: string = "Africa";
 var africa2008: number = 1028;
 var africa2018: number = 1235.5;
-var southamerica2008: number = 1132.6;
-var southamerica2018: number = 1261.5;
+//Southamerica
+var countrySouthamerica: string ="Southamerica";
+var southAmerica2008: number = 1132.6;
+var southAmerica2018: number = 1261.5;
+//Europa
+var countryEurope: string = "Europe";
 var europe2008: number = 4965.7;
 var europe2018: number = 4209.3;
-var northamerica2008: number = 6600.4;
-var northamerica2018: number = 6035.6;
+//Northamerica
+var countryNorthamerica: string = "Northamerica";
+var northAmerica2008: number = 6600.4;
+var northAmerica2018: number = 6035.6;
+//Asia
+var countryAsia: string = "Asia";
 var asia2008: number = 12954.7;
 var asia2018: number = 16274.2;
+//Australia
+var countryAustralia: string = "Australia";
 var australia2008: number = 1993;
 var australia2018: number = 2100.5;
-var all: number = africa2018 + southamerica2018 + europe2018 + northamerica2018 + asia2018 + australia2018;
+//World
+var all: number = africa2018 + southAmerica2018 + europe2018 + northAmerica2018 + asia2018 + australia2018;
 
-//Berechnet die Relativen Gesamtemissionen der Welt
-function calcGesamtemission1(zahl1: number, zahl2:number) {
-    const ergebniss: number = zahl1/zahl2*100
-    return ergebniss.toFixed(2)
-}
+//Emission
+function emission (countryName: string, emissionen2018: number, emission2008: number) {
+    document.querySelector(".country").innerHTML = countryName;
+    document.querySelector(".emissionen2018").innerHTML = emissionen2018.toString();
+    document.querySelector(".relativeToWorld").innerHTML = Math.round(emissionen2018 / all * 100 * 100) / 100 + "%"; 
+    document.querySelector(".growthRate").innerHTML = Math.round((emissionen2018 - emission2008) / emission2008 * 100 * 100 / 100) + "%";
+    document.querySelector(".growthRateAbsolute").innerHTML = `${Math.round((emissionen2018 - emission2008) * 100) / 100}`; 
+    document.querySelector<HTMLElement>(".chart").style.height = `${Math.round((emissionen2018 / all * 100 * 100) / 100)}%`;
+};
 
-//Berechnet die Veränderung 
-function calcVerändert1(zahl1: number, zahl2:number) {
-    const ergebniss: number = (zahl1-zahl2)/zahl2*100
-    return ergebniss.toFixed(2)
-}
-
-//Berechnet die Differenz 
-function calcVergleich1(zahl1: number, zahl2:number) {
-    const ergebniss: number = zahl1-zahl2
-    return ergebniss.toFixed(2)
-}
-
-console.log(`Die Emission von ${countryAfrica} ist: ${africa2018} kg CO2`);
-console.log(`Die Emission von ${countrySouthamerica} ist: ${southamerica2018} kg CO2`);
-console.log(`Die Emission von ${countryEurope} ist: ${europe2018} kg CO2`);
-console.log(`Die Emission von ${countryNorthamerica} ist: ${northamerica2018} kg CO2`);
-console.log(`Die Emission von ${countryAsia} ist: ${asia2018} kg CO2`);
-console.log(`Die Emission von ${countryAustralia} ist: ${australia2018} kg CO2`);
-
-console.log(`Relativ zu Gesamtemission der Welt verursacht ${countryAfrica} damit ${calcGesamtemission1(africa2018, all)} %`);
-console.log(`Relativ zu Gesamtemission der Welt verursacht ${countrySouthamerica} damit ${calcGesamtemission1(southamerica2018, all)} %`);
-console.log(`Relativ zu Gesamtemission der Welt verursacht ${countryEurope} damit ${calcGesamtemission1(europe2018, all)} %`);
-console.log(`Relativ zu Gesamtemission der Welt verursacht ${countryNorthamerica} damit ${calcGesamtemission1(northamerica2018, all)} %`);
-console.log(`Relativ zu Gesamtemission der Welt verursacht ${countryAsia} damit ${calcGesamtemission1(asia2018, all)} %`);
-console.log(`Relativ zu Gesamtemission der Welt verursacht ${australia2018} damit ${calcGesamtemission1(australia2018, all)} %`);
-
-console.log(`Für ${countryAfrica} hat sich 2018 im Vergleich zu 2008 die Emission um ${calcVerändert1(africa2018, africa2008)} % verändert`);
-console.log(`Für ${countrySouthamerica} hat sich 2018 im Vergleich zu 2008 die Emission ${calcVerändert1(southamerica2018, southamerica2008)} % verändert`);
-console.log(`Für ${countryEurope} hat sich 2018 im Vergleich zu 2008 die Emission um ${calcVerändert1(europe2018, europe2008)} % verändert`);
-console.log(`Für ${countryNorthamerica} hat sich 2018 im Vergleich zu 2008 die Emission um ${calcVerändert1(northamerica2018, northamerica2008)} % verändert`);
-console.log(`Für ${countryAsia} hat sich 2018 im Vergleich zu 2008 die Emission um ${calcVerändert1(asia2018, asia2008)} % verändert`);
-console.log(`Für ${countryAustralia} hat sich 2018 im Vergleich zu 2008 die Emission um ${calcVerändert1(australia2018, australia2008)} % verändert`);
-
-console.log(`2018 im Vergleich zu 2008 sind das ${calcVergleich1(africa2018, africa2008)} kg CO2`);
-console.log(`2018 im Vergleich zu 2008 sind das ${calcVergleich1(southamerica2018, southamerica2008)} kg CO2`);
-console.log(`2018 im Vergleich zu 2008 sind das ${calcVergleich1(europe2018, europe2008)} kg CO2`);
-console.log(`2018 im Vergleich zu 2008 sind das ${calcVergleich1(northamerica2018, northamerica2008)} kg CO2`);
-console.log(`2018 im Vergleich zu 2008 sind das ${calcVergleich1(asia2018, asia2008)} kg CO2`);
-console.log(`2018 im Vergleich zu 2008 sind das ${calcVergleich1(australia2018, australia2008)} kg CO2`);
+window.addEventListener("load", function() {
+    document.querySelector(".europe").addEventListener("click", function() { emission (countryEurope, europe2018, europe2008);});
+    document.querySelector(".northamerica").addEventListener("click" , function() { emission(countryNorthamerica, northAmerica2018, northAmerica2008);});
+    document.querySelector(".southamerica").addEventListener("click" , function() { emission(countrySouthamerica, southAmerica2018, southAmerica2008)})
+    document.querySelector(".africa").addEventListener("click" , function() { emission(countryAfrica, africa2018, africa2008)})
+    document.querySelector(".asia").addEventListener("click" , function() { emission(countryAsia, asia2018, asia2008)})
+    document.querySelector(".australia").addEventListener("click" , function() { emission(countryAustralia, africa2018, africa2008)})
+});
